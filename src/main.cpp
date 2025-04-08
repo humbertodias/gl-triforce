b9e0f860f755c993974c385b60065a51dcc545e1
Humberto Dias <hldtux@gmail.com>
2025-04-08 12:10:19 -0300

    add triforce

--- src/main.cpp
+++ src/main.cpp
@@ -1,173 +1,232 @@
-#ifdef _WIN32
-#define SDL_MAIN_HANDLED
-#endif
-
-#ifndef __APPLE__
-#include <GL/glew.h>
-#else
-#include <OpenGL/gl3.h>
-#endif
-
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_opengl.h>
-#include <SDL3/SDL_keycode.h>
-#include <cstdlib>
-#include <iostream>
+#include <GL/glew.h>
 #include <glm/glm.hpp>
-#include <glm/mat4x4.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
-#include "GLFunctions.h"
+#include <iostream>
+#include <cmath>
 
-void SDLErrorExit(const std::string &_msg) {
-    std::cerr << _msg << std::endl;
-    std::cerr << SDL_GetError() << std::endl;
-    SDL_Quit();
-    exit(EXIT_FAILURE);
+const GLfloat LIGHT_POSITION[] = {0.0f, 0.0f, 10.0f, 1.0f};
+const GLfloat WHITE_LIGHT[] = {1.0f, 1.0f, 1.0f, 1.0f};
+const GLfloat TRIANGLE_COLOR[] = {0.86f, 0.74f, 0.14f};
+const GLfloat SIDE_COLOR[] = {0.78f, 0.59f, 0.0f};
+
+double angle = 0.0;
+const int DEPTH = -30;
+
+void setColor(const GLfloat *color)
+{
+    glColor3f(color[0], color[1], color[2]);
 }
 
-SDL_GLContext createOpenGLContext(SDL_Window *window) {
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+void drawTriangle(float tx, float ty, float tz)
+{
+    glPushMatrix();
+    glTranslatef(tx, ty, tz);
+    glRotated(angle, 0.0f, 1.0f, 0.0f);
 
-    return SDL_GL_CreateContext(window);
+    // Front face
+    glBegin(GL_TRIANGLES);
+    setColor(TRIANGLE_COLOR);
+    glNormal3f(0, 0, 1);
+    glVertex3f(-5, -5, 1);
+    glVertex3f(0, 5, 1);
+    glVertex3f(5, -5, 1);
+    glEnd();
+
+    // Back face
+    glBegin(GL_TRIANGLES);
+    setColor(TRIANGLE_COLOR);
+    glNormal3f(0, 0, -1);
+    glVertex3f(-5, -5, -1);
+    glVertex3f(0, 5, -1);
+    glVertex3f(5, -5, -1);
+    glEnd();
+
+    // Side faces
+    glBegin(GL_QUADS);
+    setColor(SIDE_COLOR);
+
+    // Left side
+    glNormal3f(-1, 0, 0);
+    glVertex3f(-5, -5, -1);
+    glVertex3f(0, 5, -1);
+    glVertex3f(0, 5, 1);
+    glVertex3f(-5, -5, 1);
+
+    // Right side
+    glNormal3f(1, 0, 0);
+    glVertex3f(5, -5, 1);
+    glVertex3f(0, 5, 1);
+    glVertex3f(0, 5, -1);
+    glVertex3f(5, -5, -1);
+    glEnd();
+
+    glPopMatrix();
 }
 
-int main(int argc, char **argv) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        SDLErrorExit("Unable to initialize SDL");
-    }
+void setupLighting()
+{
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
 
-    SDL_Window *window = SDL_CreateWindow("SDL3 OpenGL4.x Triangle",
-                                          1024, 720,
-                                          SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
-    if (!window) {
-        SDLErrorExit("Unable to create window");
-    }
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
 
-    SDL_GLContext glContext = createOpenGLContext(window);
-    if (!glContext) {
-        SDLErrorExit("Problem creating OpenGL context");
-    }
+    glLightfv(GL_LIGHT0, GL_POSITION, LIGHT_POSITION);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, WHITE_LIGHT);
+}
 
-    SDL_GL_MakeCurrent(window, glContext);
-    SDL_GL_SetSwapInterval(1);
+void setupProjection(int width, int height)
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
 
-#ifndef __APPLE__
+    float aspect = static_cast<float>(width) / height;
+    float fov = glm::radians(60.0f);
+    float near = 0.1f;
+    float far = 100.0f;
+
+    float top = tan(fov / 2.0f) * near;
+    float bottom = -top;
+    float right = top * aspect;
+    float left = -right;
+
+    glFrustum(left, right, bottom, top, near, far);
+}
+
+void setupModelView()
+{
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void clearScreen()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+}
+
+bool initGLEW()
+{
+    glewExperimental = GL_TRUE;
     GLenum err = glewInit();
-    if (GLEW_OK != err) {
-        std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
-        exit(EXIT_FAILURE);
+    if (err != GLEW_OK)
+    {
+        std::cerr << "GLEW init failed: " << glewGetErrorString(err) << "\n";
+        return false;
     }
-    std::cerr << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << std::endl;
-#endif
+    return true;
+}
 
-    auto vaoID = createTriangle(0.8f);
-    const std::string vertex = R"(
-#version 400 core
-layout (location = 0) in vec3  inPosition;
-layout (location = 1) in vec3 inColour;
-layout (location = 2) in vec3 inNormal;
-uniform mat4 MVP;
-uniform mat4 model;
-out vec3 vertColour;
-out vec3 normal;
-out vec3 fragPos;
-void main() {
-    gl_Position = MVP * vec4(inPosition, 1.0);
-    vertColour = inColour;
-    normal = normalize(inNormal);
-    fragPos = vec3(model * vec4(inPosition, 1.0));
-})";
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cerr << "SDL Init failed: " << SDL_GetError() << "\n";
+        return 1;
+    }
 
-    const std::string fragment = R"(
-#version 400 core
-in vec3 vertColour;
-in vec3 normal;
-in vec3 fragPos;
-out vec4 fragColour;
-uniform vec3 lightPos;
-void main() {
-    vec3 s = normalize(lightPos - fragPos);
-    vec3 v = normalize(-fragPos);
-    vec3 h = normalize(v + s);
-    float sDotN = max(dot(s, normal), 0.0);
-    vec3 diffuse = vertColour * sDotN;
-    fragColour = vec4(diffuse, 1.0);
-})";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
-    auto shaderID = loadShaderFromStrings(vertex, fragment);
-    auto MVP = glGetUniformLocation(shaderID, "MVP");
-    auto model = glGetUniformLocation(shaderID, "model");
-    auto lightPos = glGetUniformLocation(shaderID, "lightPos");
+    SDL_Window *window = SDL_CreateWindow("Triforce",
+                                          800, 600,
+                                          SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
-    glUniform3f(lightPos, 0.0f, 2.0f, 0.0f);
-    glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    SDL_GL_SwapWindow(window);
+    if (!window)
+    {
+        std::cerr << "Window creation failed: " << SDL_GetError() << "\n";
+        return 1;
+    }
 
-    bool quit = false;
-    float rotation = 0.0f;
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (!glContext)
+    {
+        std::cerr << "OpenGL context creation failed: " << SDL_GetError() << "\n";
+        return 1;
+    }
+
+    if (!initGLEW())
+        return 1;
+
+    setupProjection(800, 600);
+    glEnable(GL_DEPTH_TEST);
+
+    const int targetFPS = 60;
+    const int frameDelay = 1000 / targetFPS;
+
+    bool running = true;
+    bool fullscreen = false;
+    bool wireframe = false;
     SDL_Event event;
 
-    glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 3.0f, 3.0f),
-                                 glm::vec3(0.0f, 0.0f, 0.0f),
-                                 glm::vec3(0.0f, 1.0f, 0.0f));
-    glm::mat4 project = glm::perspective(45.0f, 1024.0f / 720.0f, 0.01f, 200.0f);
+    while (running)
+    {
+        Uint32 frameStart = SDL_GetTicks();
 
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_EVENT_QUIT:
-                    quit = true;
-                    break;
-                case SDL_EVENT_WINDOW_RESIZED: {
-                    int w, h;
-                    SDL_GetWindowSizeInPixels(window, &w, &h);
-                    glViewport(0, 0, w, h);
-                    project = glm::perspective(45.0f, static_cast<float>(w) / h, 0.01f, 200.0f);
-                    break;
-                }
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_EVENT_QUIT:
+                running = false;
+                break;
+            case SDL_EVENT_WINDOW_RESIZED:
+            {
+                setupProjection(event.window.data1, event.window.data2);
+                glViewport(0, 0, event.window.data1, event.window.data2);
+                break;
+            }
 
-                // now we look for a keydown event
-                case SDL_EVENT_KEY_DOWN: {
-                    switch (event.key.key) {
-                        case SDLK_ESCAPE:
-                            break;
-                        case SDLK_W:
-                            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-                            break;
-                        case SDLK_G:
-                            SDL_SetWindowFullscreen(window, false);
-                            break;
-                        default:
-                            break;
-                    } // end of key process
-                } // end of keydown
+            // now we look for a keydown event
+            case SDL_EVENT_KEY_DOWN:
+            {
+                switch (event.key.key)
+                {
+                case SDLK_ESCAPE:
+                    running = false;
+                    break;
+                case SDLK_W:
+                    wireframe = !wireframe;
+                    if(wireframe)
+                        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+                    else
+                        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+                    break;
+                case SDLK_F:
+                    fullscreen = !fullscreen;
+                    SDL_SetWindowFullscreen(window, fullscreen);
+                    break;
+                default:
+                    break;
+                } // end of key process
+            } // end of keydown
             }
         }
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        rotation += 1.0f;
-        auto rotY = glm::rotate(glm::mat4(1.0f), glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
-        auto transform = project * view * rotY;
-        glUniformMatrix4fv(MVP, 1, GL_FALSE, glm::value_ptr(transform));
-        glUniformMatrix4fv(model, 1, GL_FALSE, glm::value_ptr(view * rotY));
+        clearScreen();
+        setupModelView();
+        setupLighting();
 
-        glBindVertexArray(vaoID);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        drawTriangle(-5, -5, DEPTH);
+        drawTriangle(5, -5, DEPTH);
+        drawTriangle(0, 5, DEPTH);
+
+        angle = fmod(angle + 1.0, 360.0);
         SDL_GL_SwapWindow(window);
+
+        Uint32 frameTime = SDL_GetTicks() - frameStart;
+        if (frameDelay > frameTime)
+        {
+            SDL_Delay(frameDelay - frameTime);
+        }
     }
 
-    glDeleteProgram(shaderID);
-    glDeleteVertexArrays(1, &vaoID);
+    SDL_GL_DestroyContext(glContext);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
-    return EXIT_SUCCESS;
+    return 0;
 }
